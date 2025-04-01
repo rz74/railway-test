6100d47a7c7a036cf1e8d2f9ff17f524f55d439a
Rex Zhang <rex.zhang@metrohm.com>
2025-04-01 13:43:59 -0400

    init

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "✅ Minimal Puzzle Backend is running"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
