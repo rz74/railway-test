4d1f825fed8f7b08508ea40b5fc5675e373ed31a
Rex Zhang <rex.zhang@metrohm.com>
2025-04-01 14:25:11 -0400

    t4

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "✅ Minimal backend is running"
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
